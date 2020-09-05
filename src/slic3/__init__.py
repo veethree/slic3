3b170c828d7d7d5eba5d31e52d7af08d28fe876d
veethree <69903178+veethree@users.noreply.github.com>
2020-09-05 23:55:01 +0000

    Update __init__.py

--- src/slic3/__init__.py
+++ src/slic3/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "cpCvdY0GfoJAIDYXCz9ChDkDBO66RPYn"
+app.config["SECRET_KEY"] = "CHANGE THIS PLEASE!"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
 db = SQLAlchemy(app)
 
